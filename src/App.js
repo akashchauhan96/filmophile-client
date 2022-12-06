5d496c6379b39c0b18aea5871fd611dd50ead6bc
Akash Chauhan <akash.sc96@gmail.com>
2022-12-06 03:10:27 -0500

    Create first draft of routing on App.js file along with the various page folder and jsx files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import UserMovieList from "./pages/UserMovieList/UserMovieList";
+import MovieHome from "./pages/MovieHome/MovieHome";
+import MovieDetails from "./pages/MovieDetails/MovieDetails";
+import AddMovie from "./pages/AddMovie/AddMovie";
+import "./App.scss";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MovieHome />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="/list" element={<UserMovieList />} />
+          <Route path="/list/new" element={<AddMovie />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
---
+++ src/pages/MovieHome/MovieHome.jsx
@@ -0,0 +1,3 @@
+export default function MovieHome() {
+  return <div>Hellow</div>;
+}
