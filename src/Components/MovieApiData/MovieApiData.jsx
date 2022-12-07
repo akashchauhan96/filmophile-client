fd1a490c6d8a01b431dafc3362edcf4c300db5ec
Akash Chauhan <akash.sc96@gmail.com>
2022-12-07 06:39:59 -0500

    Finish the pagination but need to add the page numbers in later iteration

--- src/Components/MovieApiData/MovieApiData.jsx
+++ src/Components/MovieApiData/MovieApiData.jsx
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import SingleMoviePoster from "../SingleMoviePoster/SingleMoviePoster";
+import { v4 as uuidv4 } from "uuid";
 
 export default function MovieApiData() {
   const [moviesArray, setMoviesArray] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [moviesPerPage, setMoviesPerPage] = useState(10);
-  // const [totalMovies, setTotalMovies] = useState(null);
-  // const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const axiosBaseURL = process.env.REACT_APP_TMDB_API_BASE_URL;
+  const axiosApiKey = process.env.REACT_APP_API_KEY_QUERY;
+  const axiosURL = axiosBaseURL + axiosApiKey;
 
   let items = [];
 
@@ -18,24 +21,19 @@ export default function MovieApiData() {
 
   useEffect(() => {
     let count = 1;
-    const axiosBaseURL = process.env.REACT_APP_TMDB_API_BASE_URL;
-    const axiosApi = process.env.REACT_APP_API_KEY_QUERY;
-    const axiosURL = axiosBaseURL + axiosApi;
     let movies = [];
     const getMovies = async (count) => {
       setLoading(true);
       try {
         const resp = await axios.get(
-          `${axiosBaseURL}/discover/movie${axiosApi}&page=${count}`
+          `${axiosBaseURL}/discover/movie${axiosApiKey}&page=${count}`
         );
-        // .get(`${axiosBaseURL}/discover/movie${axiosApi}&page=${count}`)
         if (count <= 4) {
           count++;
           getMovies(count);
           movies = movies.concat(resp.data.results);
         } else {
           setMoviesArray(movies);
-          // paginatedItems(itemsPerPage, movieArray);
           setLoading(false);
         }
       } catch (err) {
@@ -45,34 +43,21 @@ export default function MovieApiData() {
     getMovies(count);
   }, []);
 
-  // function paginatedItems(itemsPerPage, totalMovies) {
-  //   const endOffset = itemOffset + itemsPerPage;
-  //   console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-  //   const currentItems = items.slice(itemOffset, endOffset);
-  //   console.log(currentItems);
-  //   const pageCount = Math.ceil(items.length / itemsPerPage);
+  //Get Current Post
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  const currentMovie = moviesArray.slice(indexOfFirstMovie, indexOfLastMovie);
 
-  //   const handlePageClick = (event) => {
-  //     const newOffset = (event.selected * itemsPerPage) % items.length;
-  //     console.log(
-  //       `User requested page number ${event.selected}, which is offset ${newOffset}`
-  //     );
-  //     setItemOffset(newOffset);
-  //   };
-
-  return (
-    <>
-      <div>Hello</div>
-      <SingleMoviePoster moviesArray={moviesArray} loading={loading} />
-      {/* <ReactPaginate
-        breakLabel="..."
-        nextLabel=">"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="<"
-        renderOnZeroPageCount={null}
-      /> */}
-    </>
-  );
+  if (moviesArray.length !== 0) {
+    return (
+      <>
+        <SingleMoviePoster
+          moviesArray={currentMovie}
+          loading={loading}
+          axiosBaseURL={axiosBaseURL}
+          axiosApiKey={axiosApiKey}
+        />
+      </>
+    );
+  }
 }
--- src/Components/SingleMoviePoster/SingleMoviePoster.jsx
+++ src/Components/SingleMoviePoster/SingleMoviePoster.jsx
@@ -1,34 +1,35 @@
+import "./SingleMoviePoster.scss";
+import axios from "axios";
+
 export default function SingleMoviePoster({ moviesArray, loading }) {
+  const imageBaseURL = "https://image.tmdb.org/t/p/original";
+  const split = moviesArray[0].release_date.substring(0, 4);
+  console.log(split);
   if (loading) {
     return <h2>Loading...</h2>;
   }
+  console.log(moviesArray);
   return (
     <>
-      {moviesArray.map((movie) => {
-        <div className="movie-card">
-          <div className="movie-card__img-container">
-            <img
-              className="movie-card__movie-poster"
-              src={movie.poster_path}
-              alt={movie.title}
-            />
-          </div>
-        </div>;
-      })}
-      {/* {currentItems &&
-      currentItems.map((item) => {
-        totalMovies.map((movie) => {
-          <div className="movie-card">
-            <div className="movie-card__img-container">
-              <img
-                className="movie-card__movie-poster"
-                src={movie.poster_path}
-                alt={movie.title}
-              />
+      <section className="movie-cards">
+        {moviesArray.map((movie) => {
+          return (
+            <div className="movie-card" key={movie.id}>
+              <div className="movie-card__img-container">
+                <img
+                  className="movie-card__movie-poster"
+                  src={imageBaseURL + movie.poster_path}
+                  alt={movie.title}
+                />
+              </div>
+              <h2 className="movie-card__movie-title">{movie.title}</h2>
+              <p className="movie-card__movie-release">
+                {movie.release_date.substring(0, 4)}
+              </p>
             </div>
-          </div>;
-        });
-      })} */}
+          );
+        })}
+      </section>
     </>
   );
 }
